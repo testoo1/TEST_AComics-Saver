d0994e343bbf4f7fa56886b0eb5c605dd48682f5
Alexey Gridnev <alex-grid@mail.ru>
2017-07-14 13:54:08 +0300

    [r]: core functionality

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import os
 
 reg_URL = re.compile(r"http[s]?://((\S+.\w+)/([~\S]+))")
 
+def ERROR(text):
+    print('-'*78)
+    print("ERROR: {}".format(text))
+    print('-'*78)    
+
 def item_in_prog_config(user_item,prog_item):
     user_link = reg_URL.match(user_item['link'])
     prog_link = reg_URL.match(prog_item['link'])
@@ -18,15 +23,16 @@ def add_field(item, key, value):
         item[key] = value
 
 def add_missing_field(prog_item):
-    add_field(prog_item, 'page_first'  , 1)
+    add_field(prog_item, 'page_first'  , None)
     add_field(prog_item, 'page_current', 1)
     add_field(prog_item, 'page_last'   , None)
 
-    match_obj         = reg_URL.match(prog_item['link'])
+    add_field(prog_item, 'downloaded_in_this_session', 0)
 
-# small hack: replace already existing link  to link with discarded page number
-# example: https://acomics.ru/~4pairs/134 ->
-#          https://acomics.ru/~4pairs
+    match_obj         = reg_URL.match(prog_item['link'])
+    # small hack: replace already existing link  to link with discarded page number
+    # example: https://acomics.ru/~4pairs/134 ->
+    #          https://acomics.ru/~4pairs
     prog_item['link'] = match_obj.group(0)
 
     domain       = match_obj.group(2)
@@ -34,9 +40,15 @@ def add_missing_field(prog_item):
     add_field(prog_item, 'domain', domain)
     add_field(prog_item, 'relative_URL', relative_URL)
     
-    # TODO  
-    # if "name" not in prog_item:
-    #   download page and get name for comics
+    if 'name' not in prog_item:
+        page = request.urlopen("http://{domain}/{relUrl}".format(domain=domain,
+                                                                 relUrl=relative_URL)).read().decode('utf-8')
+        reg  = re.compile("<meta property=\"og:title\" content=\"(.+)\"")
+        try:
+            name = reg.search(page).group(1)
+        except:
+            name = prog_item['relative_URL'][1:]
+        add_field(prog_item, 'name', name)
 
 def update_config(user_config, prog_config):
     for user_item in user_config:
@@ -57,68 +69,127 @@ def set_directory(name):
         try:
             os.mkdir(name)
         except:
-            print('-'*78)
-            print("ERROR: Can't create \"{}\" folder".format(name))
-            print('-'*78)
+            ERROR("Can't create \"{}\" folder".format(name))
             return None
     return name
 
-def set_save_path(domain, name):
-    if set_directory(domain) != None:
-        path = "{domain}/{name}/".format(domain = domain, name = name)
+def set_save_path(item):
+    if set_directory(item['domain']) != None:
+        path = "{domain}/{name}/".format(domain = item['domain'],
+                                         name   = item['name'])
         if set_directory(path) != None:
             return path
     return None
 
 
-def get_page():
-    pass
-def get_image():
-    pass
-def save_image():
-    pass
-def get_next_page_URL():
-    pass
+def get_page(url):
+    try:
+        return request.urlopen(url).read().decode('utf-8')
+    except:
+        return None
 
+class Image:
+    def __init__(self, data, extension):
+        self.data      = data
+        self.extension = extension
 
-def download_comics():
-    set_save_path()
+def get_image(page,item):
+    reg = re.compile(r"id=\"mainImage\" src=\"(\S+\.(\w+))\"")
+    try:
+        result = reg.search(page)
+        image_URL = "http://{domain}{rel_URL}".format(domain =item['domain'],
+                                                      rel_URL=result.group(1))
+        extension = result.group(2)
+        return(Image(request.urlopen(image_URL).read(),extension))
+    except:
+        ERROR("Can't get image from the page")
+        return Image(None, None)
 
-    is_next_page = True
-    while is_next_page:    
-        get_page()
+def save_image(image,item):
+    try:
+        file = open("{domain}/{comics_name}/{file_name}".format(
+            domain     =item['domain'],
+            comics_name=item['name'],
+            file_name  ="{name}_{counter:03}.{ext}".format(name=item['name'],
+                                                           counter=item['page_current'],
+                                                           ext=image.extension)),'wb')
+        file.write(image.data)
+        file.close()
+    except:
+        ERROR("Can't save image \"{file_name}\"".format(\
+            file_name  ="{name}_{counter:03}.{ext}".format(name=item['name'],
+                                                           counter=item['page_current'],
+                                                           ext=image.extension)))
 
-        get_image()
-        save_image()
-        
-        get_next_page_URL()
-        # temp ->
-        is_next_page = False
-        # temp <-
+def get_next_page_URL(page):
+    try:
+        reg = re.compile(r"<a href=\"(\S+)\" class=\"button large comic-nav-next\">")
+        return reg.search(page).group(1)
+    except:
+        return None
+
+def download_comics(item):
+    if not set_save_path(item):
+        ERROR("Can't set path for comics \"{name}\"".format(name=item['name']))
+        return
+
+    if item['page_first'] != None:
+        item['page_current'] = item['page_first']
+
+    # small hack: when we start download page we set url with last saved
+    #             page value. It's help us to check that next page exist
+    #             (or we downloaded entire comics in previous session)
+    url = "https://{domain}/{name}/{page}".format(domain=item['domain'],
+                                                  name  =item['relative_URL'],
+                                                  page  =item['page_current'])
+    while url:
+        if item['page_last'] == None or item['page_current'] <= item['page_last']:
+            page  = get_page(url)
+            if page == None:
+                ERROR("Can't load page \"{url}\"".format(url=url))
+                return
+            # temp ->
+            print(url)
+            # temp <-
+            url = get_next_page_URL(page)
+
+            if url or item['downloaded_in_this_session'] != 0:
+                # if not url - we on the last page
+                # item['downloaded_in_this_session'] != 0 -- small hack:
+                #   if value == 0: we stop at this page in previous session
+                #                  and downlad image from it already
+                #   else: it's last page and we need to download it
+                image = get_image(page,item)
+                if image.data != None and image.extension != None:
+                    save_image(image,item)
+                    item['page_current'] += 1
+                    item['downloaded_in_this_session'] += 1
+        else:
+            return
+
+    item['downloaded_in_this_session'] = 0
 
 # ----------------------------------------------------------------------------
 
 def main():
-    # USER_CONFIG_FILE = "user.config"
-    # PROG_CONFIG_FILE = "prog.config"
+    USER_CONFIG_FILE = "user.config"
+    PROG_CONFIG_FILE = "prog.config"
 
-    # user_config, prog_config = config_files.load(USER_CONFIG_FILE,
-    #                                              PROG_CONFIG_FILE)
-    # update_config(user_config, prog_config)
-
-    # # temp ->
-    # config_files.config.dump(prog_config, open('prog.config','w', encoding='utf-8'))
-    # # temp <-
-    # # -----------------
-    # download_comics()
+    try:
+        user_config, prog_config = config_files.load(USER_CONFIG_FILE,
+                                                     PROG_CONFIG_FILE)
+    except:
+        ERROR("Can't open (or create) config files")
+        return        
     
-    # temp ->
-    temp_domain = "acomics.ru"
-    temp_link   = "https://acomics.ru/~4pairs"
-    temp_name   = "4 пары"
-    # temp <-
+    update_config(user_config, prog_config)
 
-    path = set_save_path(temp_domain, temp_name)
+    # -----------------
+    for item in prog_config:
+        download_comics(item)
+        config_files.config.dump(prog_config, open('prog.config','w', encoding='utf-8'))
 
+
+# ----------------------------------------------------------------------------
 if __name__ == '__main__':
     main()
